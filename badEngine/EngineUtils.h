dab71a1b7257c9155258bd89bfeedc3874cef3cd
GlobusShwobus <fjodorzaro@gmail.com>
2026-03-07 21:05:43 +0200

    helper for rect to sld rect

--- badEngine/EngineUtils.h
+++ badEngine/EngineUtils.h
@@ -1,13 +1,25 @@
 #pragma once
 
 #include <SDL3/SDL_video.h>
+#include "Float2.h"
+#include "Rect.h"
 #include "Matrix3.h"
 
 namespace badEngine
 {
 	badCore::Mat3 sdl_window_matrix(SDL_Window* const window) noexcept;
 
-	constexpr SDL_FPoint find_screen_relative(const badCore::float2& point, const badCore::Mat3& transformer) noexcept
+	constexpr SDL_FRect rect_to_sdl_frect(const badCore::Rect& rect)noexcept
+	{
+		return {
+			rect.min.x,
+			rect.min.y,
+			rect.get_width(),
+			rect.get_height()
+		};
+	}
+
+	constexpr SDL_FPoint logical_to_visual_point(const badCore::float2& point, const badCore::Mat3& transformer) noexcept
 	{
 		return
 		{
