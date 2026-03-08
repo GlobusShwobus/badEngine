5bfd46473720705a6701cfd47d5bf8ee5cd5a828
GlobusShwobus <fjodorzaro@gmail.com>
2026-03-08 20:54:40 +0200

    load data changes. intro for algos

--- badEngine/load_data.h
+++ badEngine/load_data.h
@@ -7,32 +7,30 @@
 #include "SDL3/SDL_render.h"
 
 namespace badEngine {
-	// assumes path if verified, provides no checks and can throw anything if malformed input
-	// verify before or trust
+	// basic load file into a json function. assumes complete validity thus does no checks. will throw on failure
 	nlohmann::json load_json(const char* path);
 
-	struct WindowContextDescription {
+	// a handle used to describe how a window should be initialized
+	struct WindowDescription {
 		std::string heading;
 		uint32_t width;
 		uint32_t height;
 		uint64_t flags;
 	};
 
-	// assumes path if verified, provides no checks and can throw anything if malformed input
-	// verify before or trust
-	WindowContextDescription extract_WindowContext_desc(const nlohmann::json& manifest, const char* key);
+	// extracts data from a json and creates a description for how a window should be initalized. assumes complete validity thus does no checks. will throw on failure
+	WindowDescription create_window_description(const nlohmann::json& manifest, const char* key);
 
-
+	// a handle used to describe a texture giving it a lable and a location path
 	struct TextureDescription {
 		std::string tag;
 		std::filesystem::path file;
 		//other fields?
 	};
 
-	// assumes path if verified, provides no checks and can throw anything if malformed input
-	// verify before or trust
-	badCore::Sequence<TextureDescription> extract_texture_descs(const nlohmann::json& texture_manifest, const char* key);
+	// extracts data from a json and creates descriptions for textures. assumes complete validity thus does no checks. will throw on failure
+	badCore::Sequence<TextureDescription> create_texture_descriptions(const nlohmann::json& texture_manifest, const char* key);
 
 	// loads up textures, assumes is called with the result of extract_texture_descs
-	badCore::Sequence<std::pair<std::string, badWindow::Texture>> load_textures(const badCore::Sequence<TextureDescription>& descs, SDL_Renderer* renderer);
+	badCore::Sequence<std::pair<std::string, badWindow::Texture>> load_textures(const badCore::Sequence<TextureDescription>& descs, SDL_Renderer* const renderer);
 }
