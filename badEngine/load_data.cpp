0620fe32b003646416683072264a7b7e1b99e72d
GlobusShwobus <fjodorzaro@gmail.com>
2026-03-08 20:54:58 +0200

    same as before, forgot to add cpp

--- badEngine/load_data.cpp
+++ badEngine/load_data.cpp
@@ -1,7 +1,10 @@
-
 #include "load_data.h"
 #include <fstream>
 #include "validate_data.h"
+#include "EngineUtils.h"
+#include <ranges>
+#include <execution>
+
 namespace badEngine {
 
 	nlohmann::json load_json(const char* path)
@@ -12,55 +15,50 @@ namespace badEngine {
 		return nlohmann::json::parse(file, nullptr, true, false);
 	}
 
-	// assumes the manifest was validated. provides no checks
-	WindowContextDescription extract_WindowContext_desc(const nlohmann::json& manifest, const char* key)
+	WindowDescription create_window_description(const nlohmann::json& manifest, const char* key)
 	{
 		const auto& config = manifest.at(key);
 
-		WindowContextDescription desc;
+		WindowDescription desc;
 		desc.heading = config.at("heading").get<std::string>();
-		desc.width = config.at("window_width").get<uint32_t>();
-		desc.height = config.at("window_height").get<uint32_t>();
+		desc.width   = config.at("window_width").get<uint32_t>();
+		desc.height  = config.at("window_height").get<uint32_t>();
 
 		desc.flags = 0;
-		for (const auto& flag : config.at("flags")) {
-			MapSDL_Flags_to_size_t_bitwise(flag.get<std::string>().c_str(), desc.flags);
-		}
+		for (const auto& flag : config.at("flags"))
+			json_key_to_sdl_flags(flag.get<std::string>().c_str(), desc.flags);
+
 		return desc;
 	}
 
 	// assumes the manifest is valid. provides no checks. if it indeed throws, program will be terminated.
-	badCore::Sequence<TextureDescription> extract_texture_descs(const nlohmann::json& texture_manifest, const char* key)
+	badCore::Sequence<TextureDescription> create_texture_descriptions(const nlohmann::json& texture_manifest, const char* key)
 	{
-		badCore::Sequence<TextureDescription>textures;
-
 		const auto& config = texture_manifest.at(key);
-		textures.set_capacity(config.size());
+		//make a view that makes texture descriptions and if it throws, it throws
+		auto view = config.items() | std::views::transform([](const auto& item) {
+			const auto& [tag, info] = item;
+			return TextureDescription{ tag, info.at("file").get<std::string>() };
+			}
+		);
 
-		for (const auto& [tag, info] : config.items()) {
-			textures.emplace_back( tag, info.at("file").get<std::string>() );
-		}
-
-		return textures;
+		return badCore::Sequence<TextureDescription>(std::move(view));
 	}
 
-	badCore::Sequence<std::pair<std::string, badWindow::Texture>> load_textures(const badCore::Sequence<TextureDescription>& descs, SDL_Renderer* renderer)
+	badCore::Sequence<std::pair<std::string, badWindow::Texture>> load_textures(const badCore::Sequence<TextureDescription>& descs, SDL_Renderer* const renderer)
 	{
-		badCore::Sequence<std::pair<std::string, badWindow::Texture>> textures;
+		if (!renderer)
+			return {};
 
-		if (!renderer) return textures;
-		textures.set_capacity(descs.size());
-
-		for (const auto& desc : descs) {
-			badWindow::Texture tex(desc.file.string().c_str(), renderer);
-
-			if (!tex.get()) {
-				// log maybe?
-				continue;
+		//make a view that tries to call a texture constructor and makes a pair out of a tag and texture. secondly it filters out nullptr
+		auto view = descs | std::views::transform([renderer](const TextureDescription& desc) {
+			badWindow::Texture tex = badWindow::make_texture(renderer, desc.file.string().c_str());
+			//log maybe?
+			return std::make_pair(desc.tag, std::move(tex));
 			}
-
-			textures.emplace_back(desc.tag, std::move(tex));
-		}
-		return textures;
+		) | std::views::filter([](const auto& pair) {return pair.second.get(); });
+		
+		
+		return badCore::Sequence<std::pair<std::string, badWindow::Texture>>(std::move(view));
 	}
 }
