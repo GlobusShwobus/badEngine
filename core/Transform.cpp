cff4808d067a022b342dff6f8fe6c7c14e291579
GlobusShwobus <fjodorzaro@gmail.com>
2026-03-09 11:53:29 +0200

    making transform into a basic pod-ish type

--- core/Transform.cpp
+++ core/Transform.cpp
@@ -5,14 +5,18 @@ namespace badCore
 {
 	Mat3 Transform::transform()const noexcept
 	{
-		//ORDER MATTERS
-		return badCore::Mat3::translation(mPos) * badCore::Mat3::rotation(mAngle) * badCore::Mat3::scale(mScale, mScale);
+		return
+			badCore::Mat3::translation(pos) *
+			badCore::Mat3::rotation(radians) *
+			badCore::Mat3::scale(scale, scale);
 	}
 
 	Mat3 Transform::transform_inverse()const noexcept
 	{
-		const float invScale = 1.0f / mScale;
-		//camera transform are inverse of object transforms (perspective)
-		return Mat3::scale(invScale, invScale) * Mat3::rotation(-mAngle) * Mat3::translation(-mPos);
+		const float invScale = 1.0f / scale;
+		return
+			Mat3::scale(invScale, invScale) *
+			Mat3::rotation(-radians) *
+			Mat3::translation(-pos);
 	}
 }
--- core/Transform.h
+++ core/Transform.h
@@ -5,65 +5,19 @@
 
 namespace badCore
 {
-	class Transform final
+	struct Transform 
 	{
-	public:
-		
-		Transform() = default;
-
-		constexpr const float2& get_pos()const noexcept
+		Transform(const float2& pos, float scale, float radians)
+			:pos(pos), scale(scale), radians(radians)
 		{
-			return mPos;
 		}
-
-		constexpr void move_by(const float2& offset) noexcept
-		{
-			mPos += offset;
-		}
-
-		constexpr void set_pos(const float2& pos)noexcept
-		{
-			mPos = pos;
-		}
-
-		constexpr float get_scale()const noexcept
-		{
-			return mScale;
-		}
-
-		constexpr void scale_by(float scalar) noexcept
-		{
-			mScale *= scalar;
-		}
-
-		constexpr void set_scale(float scale)noexcept
-		{
-			mScale = scale;
-		}
-
-		constexpr float get_angle()const noexcept
-		{
-			return mAngle;
-		}
-
-		constexpr void rotate_by(float degrees)noexcept
-		{
-			mAngle += degrees;
-		}
-
-		constexpr void set_angle(float angle)noexcept
-		{
-			mAngle = angle;
-		}
-
 
 		Mat3 transform()const noexcept;
 
 		Mat3 transform_inverse()const noexcept;
 
-	private:
-		class float2 mPos;
-		float mScale = 1.0f;
-		float mAngle = 0;
+		class float2 pos;
+		float scale;
+		float radians;;
 	};
 }
