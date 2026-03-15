ebcc424701d5aa7a2e68a9df9630731591c59f0e
GlobusShwobus <fjodorzaro@gmail.com>
2026-03-15 21:02:22 +0200

    docs

--- core/Make_Shape.cpp
+++ core/Make_Shape.cpp
@@ -5,10 +5,10 @@
 
 namespace badCore
 {
-	Sequence<float2> make_poly(float outerRadius, float innerRadius, int nFlares)
+	Sequence<Point> make_poly(float outerRadius, float innerRadius, int nFlares)
 	{
 		assert(nFlares >= 1);
-		Sequence<float2> polyline;
+		Sequence<Point> polyline;
 		polyline.reserve(static_cast<std::size_t>(nFlares * 2));
 
 		const float dTheta = 2.0f * 3.14159f / float(nFlares * 2);
@@ -25,6 +25,10 @@ namespace badCore
 
 	Sequence<float2> make_circle(const float2& center, float radius)
 	{
+		// an esimation of how many points are required.
+		// radius * 8 over shoots
+		// 35/49 is an approximation of how many pointers per radius unit, which is 0.714....
+		// still a estimation but good enough.
 		int size = radius * 8 * 35 / 49;
 		int arrsize = (size + (8 - 1)) & -8;
 
@@ -33,24 +37,34 @@ namespace badCore
 
 		const int32_t diameter = (radius * 2);
 
-		int32_t x = (radius - 1);
-		int32_t y = 0;
-		int32_t tx = 1;
-		int32_t ty = 1;
-		int32_t error = (tx - diameter);
+		// using the modpoint algorithm bullshit
+		int32_t x = (radius - 1); // x starts near the rightmost point of the circle 
+		int32_t y = 0; //y starts top most point of the circle
+		int32_t tx = 1;  // increment trackers
+		int32_t ty = 1;	 // increment trackers
+		int32_t error = (tx - diameter); // decision variable that tells which variable to increment next step
 
 		while (x >= y)
 		{
-			// Each of the following renders an octant of the circle
+			// each point on a circle has a point directly opposite of it. octant/circle symetry.
+			// because of this Point(x , y) only need to be set once, then mirroring them we get 8 points per loop.
 			circle.emplace_back(float2(center.x + x, center.y - y));
 			circle.emplace_back(float2(center.x + x, center.y + y));
+
 			circle.emplace_back(float2(center.x - x, center.y - y));
 			circle.emplace_back(float2(center.x - x, center.y + y));
+
 			circle.emplace_back(float2(center.x + y, center.y - x));
 			circle.emplace_back(float2(center.x + y, center.y + x));
+
 			circle.emplace_back(float2(center.x - y, center.y - x));
 			circle.emplace_back(float2(center.x - y, center.y + x));
 
+
+			// ensure the resterized circle stays as close as possible to a true circle
+			// if err <= 0; y needs to move up to stay close to the circle -> ty keeps track of the change in y dir
+			// if err > 0; x needs to move left to stay close to the circle -> tx keeps track of the change in x dir
+			// tx and ty are incremented by 2 from the derivatives of the circle equation, but as integers...
 			if (error <= 0)
 			{
 				++y;
--- core/Make_Shape.h
+++ core/Make_Shape.h
@@ -6,6 +6,19 @@
 
 namespace badCore
 {
+	/**
+	* Computes the bounding rectangle containing both input rectangles.
+	*
+	* The resulting rectangle is the minimal axis-aligned rectangle that
+	* encloses both \p a and \p b. This operation is commonly used when
+	* constructing or updating bounding volume hierarchies (BVH).
+	*
+	* \param a First rectangle.
+	* \param b Second rectangle.
+	* \return Rectangle that encloses both input rectangles.
+	* 
+	* \note no use case for it anymore atm...
+	*/
 	constexpr Rect make_union_rect(const Rect& a, const Rect& b)noexcept
 	{
 		const float minx = core_min(a.min.x, b.min.x);
@@ -16,7 +29,31 @@ namespace badCore
 		return { {minx, miny},{maxx, maxy} };
 	}
 
-	Sequence<float2> make_poly(float outerRadius, float innerRadius, int nFlares);
+	/**
+	* Generates a radial polyline with alternating outer and inner vertices.
+	* 
+	* The function produces a star-like shape by alternating between outer and inner radii
+	* while stepping uniformly around a circle. The result contains 2x flares amount of vertices.
+	* 
+	* \param outerRadius Radius of the outer vertices.
+	* \param innerRadius Radius of the inner vertices.
+	* \param nFlares Number of flares (spikes) in the resulting shape.
+	* 
+	* \return Sequence of points resembling a star containing 2x Flares aount of vertices.
+	*/
+	Sequence<Point> make_poly(float outerRadius, float innerRadius, int nFlares);
+
+
+	//#############################################################################################
+	// under construction due not being sure this is how i wanna do it...
+	// 
+	// Generates a polyline around a center point with a given radius.
+	//
+	// The function uses integer based approximates for point count finding and making the circle.
+	//#############################################################################################
 
 	Sequence<float2> make_circle(const float2& center, float radius);
+	
+	//#############################################################################################
+	//#############################################################################################
 }
