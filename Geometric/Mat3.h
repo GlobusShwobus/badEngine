593529132fd06d5ba64f8c0e2e048c8679674118
GlobusShwobus <fjodorzaro@gmail.com>
2026-03-23 00:23:48 +0200

    mat3 revision- docs tomorrow

--- Geometric/Mat3.h
+++ Geometric/Mat3.h
@@ -6,55 +6,77 @@ namespace bad
 {
 	struct Mat3
 	{
-		float a = 1, b = 0;
-		float c = 0, d = 1;
-		float tx = 0, ty = 0;
+		// | Ax  Bx  Tx |
+		// | Ay  By  Ty |
+		// |  0   0   1 |
 
-		constexpr Mat3 operator*(const Mat3& rhs)const noexcept
+		float Ax = 1, Bx = 0, Tx = 0;
+		float Ay = 0, By = 1, Ty = 0;
+
+
+		constexpr bad::Point operator*(const bad::Point& v)const noexcept
 		{
-			Mat3 r;
-			r.a = a * rhs.a + b * rhs.c;
-			r.b = a * rhs.b + b * rhs.d;
-			r.c = c * rhs.a + d * rhs.c;
-			r.d = c * rhs.b + d * rhs.d;
-
-			r.tx = a * rhs.tx + b * rhs.ty + tx;
-			r.ty = c * rhs.tx + d * rhs.ty + ty;
-			return r;
-		}
-
-		constexpr float2 transform(const float2& v)const noexcept
-		{
-			return {
-				a * v.x + b * v.y + tx,
-				c * v.x + d * v.y + ty
+			return bad::Point{ 
+				Ax * v.x + Bx * v.y + Tx,  //Tx * 1 since the third row is implied
+				Ay * v.x + By * v.y + Ty   //Ty * 1 since the third row is implied
 			};
 		}
 
-		constexpr static Mat3 translation(float x, float y) noexcept
+		constexpr Mat3 operator*(const bad::Mat3& rhs)const noexcept
 		{
 			Mat3 m;
-			m.tx = x;
-			m.ty = y;
+
+			m.Ax = Ax * rhs.Ax + Bx * rhs.Ay;
+			m.Ay = Ay * rhs.Ax + By * rhs.Ay;
+
+			m.Bx = Ax * rhs.Bx + Bx * rhs.By;
+			m.By = Ay * rhs.Bx + By * rhs.By;
+
+			m.Tx = Ax * rhs.Tx + Bx * rhs.Ty + Tx;
+			m.Ty = Ay * rhs.Tx + By * rhs.Ty + Ty;
+
 			return m;
 		}
 
-		constexpr static Mat3 scale(float sx, float sy)noexcept
+		constexpr Mat3& operator*=(const bad::Mat3& rhs) noexcept
+		{
+			return *this = *this * rhs;
+		}
+
+		constexpr static Mat3 identity() noexcept
+		{
+			return Mat3{};
+		}
+
+		constexpr static Mat3 scale(float factor) noexcept
 		{
 			Mat3 m;
-			m.a = sx;
-			m.d = sy;
+			m.Ax = factor;
+			m.By = factor;
 			return m;
 		}
 
-		constexpr static Mat3 rotation(float s, float c)noexcept
+		//assumes sin cos are pre calculated
+		constexpr static Mat3 rotate(float sin, float cos) noexcept
 		{
 			Mat3 m;
-			m.a = c;
-			m.b = -s;
-			m.c = s;
-			m.d = c;
+			m.Ax = cos;  m.Ay = sin;
+			m.Bx = -sin; m.By = cos;
+			return m;
+		}
 
+		constexpr static Mat3 translate(float x, float y) noexcept
+		{
+			Mat3 m;
+			m.Tx = x;
+			m.Ty = y;
+			return m;
+		}
+
+		constexpr static Mat3 flip_y() noexcept
+		{
+			Mat3 m;
+			m.By = -1;
 			return m;
 		}
 	};
