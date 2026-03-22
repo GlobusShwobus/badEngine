ceb00569612216971da77e0e882f04ca7c189f6a
GlobusShwobus <fjodorzaro@gmail.com>
2026-03-22 15:43:19 +0200

    one more shape

--- badEngine/MakeShape.h
+++ badEngine/MakeShape.h
@@ -2,7 +2,7 @@
 
 #include "Float2.h"
 #include "Sequence.h"
-
+#include <array>
 namespace bad
 {
 	/**
@@ -18,4 +18,25 @@ namespace bad
 	* \return Sequence of points resembling a star containing 2x Flares aount of vertices.
 	*/
 	bad::Sequence<bad::Point> make_poly(float outerRadius, float innerRadius, int nFlares);
+
+	/**
+	* Generates a 4 point shape object. Top left is always at { 0 , 0 }.
+	* 
+	* \param width the width of the shape
+	* \param height the height of the shape
+	* 
+	* \returns an array of 4 points.
+	*/
+	bad::Sequence<bad::Point> make_4ptsShape_heap(float width, float height);
+
+
+	/**
+	* Generates a 4 point shape object. Top left is always at { 0 , 0 }.
+	*
+	* \param width the width of the shape
+	* \param height the height of the shape
+	*
+	* \returns an array of 4 points.
+	*/
+	std::array<bad::Point, 4> make_4ptsShape_stack(float width, float height);
 }
