3261548d0f27704247c30f97bc171ffac476f0eb
GlobusShwobus <fjodorzaro@gmail.com>
2026-03-23 17:40:54 +0200

    made a header just for constants

---
+++ Geometric/MathConstants.h
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace bad
+{
+	static constexpr float EPSILON = 0.0001f;
+	static constexpr float PI = 3.14159265358979323846f;
+	static constexpr float TAU = 6.28318530717958647692f;
+}
